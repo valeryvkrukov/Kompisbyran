b24d0d572be29744009a23962238c200f50819a7
Valery V. Krukov <valery.v.krukov@gmail.com>
2016-03-04 14:24:37 -0500

    =Profile and filter form models and base view of 2nd step

---
+++ src/AppBundle/Form/MatchFilterType.php
@@ -0,0 +1,91 @@
+<?php 
+
+namespace AppBundle\Form;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MatchFilterType extends AbstractType{
+	protected $em;
+	
+	public function __construct(EntityManager $em){
+		$this->em=$em;
+	}
+	
+	public function buildForm(FormBuilderInterface $builder,array $options){
+		$builder
+			->addEventListener(FormEvents::PRE_SET_DATA,[$this,'onPreSetData']);
+	}
+	
+	public function onPreSetData(FormEvent $event){
+		$form=$event->getForm();
+		$data=$event->getData();
+		$categories=[];
+		$areas=[];
+		foreach($this->em->getRepository('AppBundle:Category')->findAll() as $cat){
+			$categories[$cat->getId()]=$cat->getName();
+		}
+		foreach($this->em->getRepository('AppBundle:Municipality')->findAll() as $area){
+			$areas[$cat->getId()]=$area->getName();
+		}
+		$form
+			->add('category','choice',[
+				'label'=>'Interests',
+				'placeholder'=>'All',
+				'choices'=>$categories,
+				'mapped'=>false
+			])
+			->add('ageFrom','choice',[
+				'label'=>'Age From',
+				'choices'=>range(18,100),
+				'mapped'=>false
+			])
+			->add('ageTo','choice',[
+				'label'=>'Age To',
+				'choices'=>range(18,100),
+				'mapped'=>false
+			])
+			->add('gender','choice',[
+				'label'=>'Gender',
+				'placeholder'=>'All',
+				'choices'=>['M'=>'Man','W'=>'Woman'],
+				'mapped'=>false
+			])
+			->add('hasChildren','choice',[
+				'label'=>'Children',
+				'placeholder'=>'Doesn\'t matter',
+				'choices'=>['0'=>'No','1'=>'Yes'],
+				'mapped'=>false
+			])
+			->add('country','country',[
+				'label'=>'Country',
+				'placeholder'=>'All',
+				'mapped'=>false
+			])
+			->add('municipality','choice',[
+				'label'=>'Area',
+				'placeholder'=>'All',
+				'choices'=>$areas,
+				'mapped'=>false
+			])
+			->add('type','choice',[
+				'label'=>'Type',
+				'placeholder'=>'Any',
+				'choices'=>['0'=>'Fikabuddy','1'=>'Musicbuddy'],
+				'mapped'=>false
+			]);
+	}
+	
+	public function setDefaultOptions(OptionsResolverInterface $resolver){
+		parent::setDefaultOptions($resolver);
+	}
+	
+	public function getName(){
+		return 'matchFilter';
+	}
+	
+}
---
+++ src/AppBundle/Form/MatchProfileType.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use AppBundle\Enum\Countries;
+
+class MatchProfileType extends AbstractType{
+	
+	public function buildForm(FormBuilderInterface $builder,array $options){
+		$builder
+			->addEventListener(FormEvents::PRE_SET_DATA,[$this,'onPreSetData'])
+			->addEventListener(FormEvents::PRE_SUBMIT,[$this,'onPreSubmit']);
+	}
+	
+	public function onPreSetData(FormEvent $event){
+		$form=$event->getForm();
+		$data=$event->getData();
+		$fullName=trim(sprintf('%s %s',$data->getFirstName(),$data->getLastName()));
+		$category=($data->getWantToLearn()==0?'Established':'New');
+		$type=($data->isMusicFriend()?'Musicbuddy':'Fikabuddy');
+		$from=Countries::getName($data->getFrom());
+		$childs=($data->hasChildren()?'Yes':'No');
+		$interests=[];
+		foreach($data->getCategories() as $cat){
+			$interests[]=$cat->getName();
+		}
+		$form
+			->add('fullName','text',['label'=>'Name','data'=>$fullName,'mapped'=>false])
+			->add('email','email',['label'=>'Email'])
+			->add('age','number',['label'=>'Age'])
+			->add('wantToLearn','text',['label'=>'Category','data'=>$category])
+			->add('from','text',['label'=>'Country','data'=>$from])
+			->add('district','text',['label'=>'Area'])
+			->add('hasChildren','text',['label'=>'Children','data'=>$childs])
+			->add('musicFriend','text',['label'=>'Type','data'=>$type])
+			->add('categories','choice',[
+				'label'=>'Interests',
+				'expanded'=>true,
+				'multiple'=>true,
+				'mapped'=>false,
+				'choices'=>$interests
+			])
+			->add('about','textarea',['label'=>'Description'])
+			->add('comment','textarea',[
+				'label'=>'Match request',
+				'mapped'=>false
+			])
+			->add('internalComment','textarea',['label'=>'Internal comments']);
+	}
+	
+	public function onPreSubmit(FormEvent $event){
+		$form=$event->getForm();
+		$data=$event->getData();
+		
+	}
+	
+	public function setDefaultOptions(OptionsResolverInterface $resolver){
+		parent::setDefaultOptions($resolver);
+	}
+	
+	public function getName(){
+		return 'matchProfile';
+	}
+	
+}
