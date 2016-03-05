f2577426b29d9479d26dff3082a2112e0820494a
Valery V. Krukov <valery.v.krukov@gmail.com>
2016-03-05 13:31:18 -0500

    Match score calculation

--- src/AppBundle/Controller/Admin/AjaxController.php
+++ src/AppBundle/Controller/Admin/AjaxController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Enum\Countries;
 
 /**
  * @Route("admin/ajax")
@@ -31,6 +32,57 @@ class AjaxController extends Controller{
 				$response['status']='zero';
 				$response['message']=$_translator->trans('requests.zero.message');
 			}
+		}
+		return new JsonResponse($response);
+	}
+	
+	/**
+	 * @Route("/get-matches",name="ajax_get_match_results")
+	 */
+	public function getMatchesAction(){
+		$request=$this->getRequest();
+		$_translator=new Translator($request->getLocale());
+		$response=[
+			'status'=>'fail'
+		];
+		$em=$this->getDoctrine()->getEntityManager();
+		$users=$em->getRepository('AppBundle:User')->getMatchedUsers($request->request->get('user_id'));
+		$currentCategories=[];
+		foreach($users['user']->getCategories() as $ccat){
+			$currentCategories[]=$ccat->getId();
+		}
+		$response['users']=[];
+		if(isset($users['result'])&&sizeof($users['result'])>0){
+			foreach($users['result'] as $u){
+				$matches=[];
+				$matches[]=(isset($u['age'])&&($u['age']==$users['user']->getAge()))?'<span class="matches">'.$u['age'].'</span>':$u['age'];
+				$matches[]=(isset($u['from'])&&($u['from']==$users['user']->getFrom()))?'<span class="matches">'.Countries::getName($u['from']).'</span>':Countries::getName($u['from']);
+				$matches[]=(isset($u['hasChildren'])&&($u['hasChildren']==$users['user']->hasChildren()))?'<span class="matches">'.($users['user']->hasChildren()?'kids':'no kids').'</span>':($users['user']->hasChildren()?'kids':'no kids');
+				$interests=[];
+				$_u=$em->getRepository('AppBundle:User')->find($u['id']);
+				foreach($_u->getCategories() as $cat){
+					$interests[]=in_array($cat->getId(),$currentCategories)?'<span class="matches">'.$cat->getName().'</span>':$cat->getName();
+				}
+				$row='<div class="row candidate">';
+				$row.='<div class="col-md-1">';
+				$row.='<p class="score">'.$u['summary_points'].'</p>';
+				$row.='</div>';
+				$row.='<div class="col-md-9 presentation">';
+				$row.='<div class="pull-right">';
+				$row.='<a href="" class="btn btn-orange">'.$_translator->trans('Edit Profile').'</a>';
+				$row.='</div>';
+				$row.='<h4>'.trim(sprintf('%s %s',$u['firstName'],$u['lastName'])).' ('.implode(', ',$matches).')</h4>';
+				$row.='<p class="interests">'.$_translator->trans('Matching interests').' : '.implode(', ',$interests).'</p>';
+				$row.='<p>'.$u['about'].'</p>';
+				$row.='</div>';
+				$row.='<div class="col-md-2 choice">';
+				$row.='<p><strong>'.$_translator->trans('Choose candidate').'</strong></p>';
+				$row.='<input type="radio" name="gender" value="1">';
+				$row.='</div>';
+				$row.='</div>';
+				$response['users'][]=$row;
+			}
+			$response['status']='ok';
 		}
 		return new JsonResponse($response);
 	}
--- src/AppBundle/Entity/UserRepository.php
+++ src/AppBundle/Entity/UserRepository.php
@@ -25,4 +25,37 @@ class UserRepository extends EntityRepository
 
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
+    
+    public function getMatchedUsers($userId){
+    	$em=$this->getEntityManager();
+    	$user=$em->getRepository('AppBundle:User')->find($userId);
+    	$dql='SELECT DISTINCT u.id,u.firstName,u.lastName,u.username,u.age,
+    			u.gender,u.from,u.about,u.hasChildren,
+    			(	
+    				COUNT(cat.id)+
+    				(CASE WHEN(u.municipality=:municipality) THEN 2 ELSE 0 END)+
+    				(CASE WHEN((u.age-:age)<5) THEN 2 ELSE 0 END)+
+    				(CASE WHEN(u.gender=:gender) THEN 1 ELSE 0 END)+
+    				(CASE WHEN((u.hasChildren>0) AND (1>:children)) THEN 2 ELSE 0 END)
+    			) AS summary_points
+    		FROM AppBundle:User u
+    			LEFT JOIN u.categories cat
+    		WHERE u.id <> :userId AND u.wantToLearn <> :wantToLearn
+    			ORDER BY summary_points DESC
+    	';
+    	$result=[
+    		'result'=>$em->createQuery($dql)
+	    		->setParameters([
+	    			'userId'=>$user->getId(),
+	    			'municipality'=>$user->getMunicipality(),
+	    			'gender'=>$user->getGender(),
+	    			'age'=>$user->getAge(),
+	    			'children'=>$user->hasChildren(),
+	    			'wantToLearn'=>$user->getWantToLearn()
+	    		])
+	    		->getResult(),
+    		'user'=>$user
+    	];
+    	return $result;
+    }
 }
